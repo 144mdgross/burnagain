7f602065449f79df1dac11558ff1ce9921af3027
144mdgross <144.mdgross@gmail.com>
2017-04-08 21:51:58 -0600

    read, read individual and update all handled.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  knex('collection')
+  .then(cd => {
+    res.render('index', { cd: cd });
+  })
+  .catch(err => {
+    console.error(err);
+  })
+
 });
+
+router.get('/:id', (req, res, next) => {
+  let id = +req.params.id
+
+  knex('collection')
+  .where('id', id)
+  .then(cd => {
+    res.render('single', {single: cd})
+  })
+})
+
+router.patch('/:id', (req, res, next) => {
+  let id = +req.params.id
+
+  knex('collection')
+  .where('id', id)
+  .update({
+    artist: req.body.artist,
+    album: req.body.album
+  })
+  .then(update => {
+    console.log(update, "success");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
 
 module.exports = router;
